b2d36118353c6acf65e746ed7426a48b60a8e5b6
Sam-Banankhu <sambanankhumhango@gmail.com>
2023-09-02 20:07:59 +0200

    emai validation

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,13 +1,22 @@
 from datetime import datetime
+from sqlalchemy.orm import validates
+import re
 from backend.app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+
+
+    @validates('username')
+    def validate_username(self, key, value):
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", value):
+            raise ValueError("Username must be a valid email address")
+        return value
 
 class Query(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
     query_text = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
